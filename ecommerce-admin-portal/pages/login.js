bf60bc61969175b6dfa5f85d341e49f11bc932ad
Sudeep Shrestha <sudeepstha.96@gmail.com>
2023-05-17 08:25:28 +0545

    feat: Login & Register

---
+++ ecommerce-admin-portal/pages/login.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const login = () => {
+  return (
+    <>
+      <div className="w-1/5 m-auto my-16 h-full p-3 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
+        <h2 className="text-center text-red-600 font-bold text-3xl font-mono">Log-In</h2>
+        <div className="flex flex-col justify-center">
+          <label>Email:</label>
+          <input
+            type="email"
+            className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+            placeholder="you@example.com"
+          />
+          <label>Password:</label>
+          <input
+            type="password"
+            className="block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          />
+        <button className="rounded px-3 py-3 rounded-3xl bg-orange-400 mt-1 w-50 border border-black-300 hover:bg-orange-500 font-semibold">Login</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default login;
---
+++ ecommerce-admin-portal/pages/register.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const register = () => {
+  return (
+    <>
+      <div className="w-2/5 p-5 mx-auto my-16 h-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
+        <div className=" text-red-600 font-bold text-3xl font-mono text-center ">
+          <h3>Register</h3>
+          <span className="font-semibold text-xl">Create an account</span>
+        </div>
+        <div className="flex gap-4 justify-center">
+          <div>
+            <label>Full-Name:</label>
+            <input
+              type="text"
+              placeholder="Enter Your Full-Name"
+              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+            />
+          </div>
+          <div>
+            <label>Username:</label>
+            <input
+              type="text"
+              placeholder="Enter Username"
+              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+            />
+          </div>
+        </div>
+        <div className="flex gap-2 justify-center">
+          <div>
+            <label>Email:</label>
+            <input
+              type="email"
+              placeholder="you@example.com"
+              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+            />
+          </div>
+        </div>
+            <div className="flex justify-center">
+            <button className="mt-2 bg-orange-400 px-5 py-2 rounded-3xl hover:bg-orange-500">
+              Register
+            </button>
+            </div>
+      </div>
+    </>
+  );
+};
+
+export default register;
